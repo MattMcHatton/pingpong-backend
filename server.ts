5340695ad25afde455fe5359eb30dc593188c789
MattMcHatton <53062304+MattMcHatton@users.noreply.github.com>
2022-10-12 10:12:20 -0500

    testing

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ app.use(cors(corsOptions));
 
 app.route("/").get((req, res) => {
   res.status(200);
-  res.send("Hello World!");
+  res.send("Hello World!!");
 });
 
 app.route("/myendpoint").get((req, res) => {
